6a84fb6a23f7fc5dc8d8d819d3986c67eaca8e9d
Alex Downey <adowney5@studentmail.nait.ca>
2024-05-13 15:07:06 -0600

    Reworked to fit with new custom button class
    - Also added a timer for UI updates

--- DAW/DAW/DAW_UI.cs
+++ DAW/DAW/DAW_UI.cs
@@ -1,41 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DAW
 {
     public partial class DAW_UI : Form
     {
-
-        private Rectangle closeButtonRect;
-        private Rectangle minimizeButtonRect;
-        private Rectangle maximizeButtonRect;
-        private bool isMaximized = false;
-        private Point lastLocation;
-        private bool isMouseOverClose = false;
-        private bool isMouseOverMinimize = false;
-        private bool isMouseOverMaximize = false;
+        private List<CustomButton> buttons;
+        private Audio newTrack;
+        private Point lastLocation; // Declare lastLocation variable here
+        private Timer uiUpdateTimer;
 
         public DAW_UI()
         {
-           FormBorderStyle = FormBorderStyle.None;
-           DoubleBuffered = true;
-           StartPosition = FormStartPosition.CenterScreen;
-           MinimumSize = new Size(1000, 600);
+            // Set form properties
+            FormBorderStyle = FormBorderStyle.None;
+            DoubleBuffered = true;
+            StartPosition = FormStartPosition.CenterScreen;
+            MinimumSize = new Size(1000, 600);
 
-           BackgroundImage = Properties.Resources.DAWspace;
-           BackgroundImageLayout = ImageLayout.Stretch;
+            // Set background image
+            BackgroundImage = Properties.Resources.DAWspace;
+            BackgroundImageLayout = ImageLayout.Stretch;
 
-           MouseDown += MainForm_MouseDown;
-           MouseMove += MainForm_MouseMove;
-           MouseMove += MainForm_MouseMoveButtons;
-           MouseLeave += MainForm_MouseLeaveButtons;
+            // Initialize custom buttons
+            buttons = new List<CustomButton>();
+
+            // Define button properties
+            int buttonWidth = 35;
+            int buttonHeight = 20;
+            int buttonSpacing = 10;
+
+            // Create window control buttons
+            buttons.Add(new CustomButton("X", new Rectangle(ClientSize.Width - buttonWidth, 0, buttonWidth, buttonHeight), Color.Black, Color.Red));
+            buttons.Add(new CustomButton("_", new Rectangle(ClientSize.Width - 3 * buttonWidth, 0, buttonWidth, buttonHeight), Color.Black, Color.Gray));
+            buttons.Add(new CustomButton("▢", new Rectangle(ClientSize.Width - 2 * buttonWidth, 0, buttonWidth, buttonHeight), Color.Black, Color.Gray));
+
+            // Create audio control buttons
+            buttons.Add(new CustomButton("■", new Rectangle((ClientSize.Width / 2) - (buttonWidth + buttonSpacing), 0, buttonWidth, buttonHeight), Color.Black, Color.Red));
+            buttons.Add(new CustomButton("▶", new Rectangle((ClientSize.Width / 2), 0, buttonWidth, buttonHeight), Color.Black, Color.Green));
+            buttons.Add(new CustomButton("●", new Rectangle((ClientSize.Width / 2) + (buttonWidth + buttonSpacing), 0, buttonWidth, buttonHeight), Color.Black, Color.Red));
+
+            // Add button click events
+            buttons[0].Click += (s, e) => Close();
+            buttons[1].Click += (s, e) => WindowState = FormWindowState.Minimized;
+            buttons[2].Click += (s, e) =>
+            {
+                if (WindowState == FormWindowState.Maximized)
+                {
+                    WindowState = FormWindowState.Normal;
+                }
+                else
+                {
+                    WindowState = FormWindowState.Maximized;
+                }
+                Invalidate();
+            };
+
+            buttons[3].Click += (s, e) => { Console.WriteLine("Stop button clicked"); StopButton(); };
+            buttons[4].Click += (s, e) => { Console.WriteLine("Play button clicked"); PlayButton(); };
+            buttons[5].Click += (s, e) => { Console.WriteLine("Record button clicked"); RecordButton(); };
+
+            // Event handlers for mouse actions
+            MouseDown += MainForm_MouseDown;
+            MouseMove += MainForm_MouseMove;
+            MouseMove += MainForm_MouseMoveButtons;
+            MouseLeave += MainForm_MouseLeaveButtons;
+            MouseClick += MainForm_MouseClick;
+
+            // Initialize and start the timer
+            uiUpdateTimer = new Timer();
+            uiUpdateTimer.Interval = 100; // 100 milliseconds
+            uiUpdateTimer.Tick += UiUpdateTimer_Tick;
+            uiUpdateTimer.Start();
+        }
+
+        private void UiUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            // Place code here to update the UI as needed every 100ms
+            // For example, you might want to refresh the form:
+            Invalidate();
+        }
+
+        private void StopButton()
+        {
+            buttons[5].DefaultColor = Color.Black;
+            newTrack?.StopRecording();
+
+            
+        }
+
+        private void PlayButton()
+        {
+            Console.WriteLine("Inside Play button");  
+        }
+
+        private void RecordButton()
+        {
+            buttons[5].DefaultColor = Color.Red;
+            newTrack = new Audio();
+            newTrack.Record();
         }
 
         private void MainForm_MouseDown(object sender, MouseEventArgs e)
@@ -50,157 +115,43 @@ namespace DAW
         {
             if (e.Button == MouseButtons.Left)
             {
-               Location = new Point(
-                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
-
-               Update();
+                Location = new Point(
+                    (Location.X - lastLocation.X) + e.X,
+                    (Location.Y - lastLocation.Y) + e.Y);
+                Update();
             }
         }
 
         private void MainForm_MouseMoveButtons(object sender, MouseEventArgs e)
         {
-            bool needsRedraw = false;
-
-            if (closeButtonRect.Contains(e.Location))
+            foreach (var button in buttons)
             {
-                if (!isMouseOverClose)
-                {
-                    isMouseOverClose = true;
-                    needsRedraw = true;
-                }
-            }
-            else
-            {
-                if (isMouseOverClose)
-                {
-                    isMouseOverClose = false;
-                    needsRedraw = true;
-                }
-            }
-
-            if (minimizeButtonRect.Contains(e.Location))
-            {
-                if (!isMouseOverMinimize)
-                {
-                    isMouseOverMinimize = true;
-                    needsRedraw = true;
-                }
-            }
-            else
-            {
-                if (isMouseOverMinimize)
-                {
-                    isMouseOverMinimize = false;
-                    needsRedraw = true;
-                }
-            }
-
-            if (maximizeButtonRect.Contains(e.Location))
-            {
-                if (!isMouseOverMaximize)
-                {
-                    isMouseOverMaximize = true;
-                    needsRedraw = true;
-                }
-            }
-            else
-            {
-                if (isMouseOverMaximize)
-                {
-                    isMouseOverMaximize = false;
-                    needsRedraw = true;
-                }
-            }
-
-            if (needsRedraw)
-            {
-                Invalidate();
+                button.OnMouseMove(e.Location);
             }
         }
 
         private void MainForm_MouseLeaveButtons(object sender, EventArgs e)
         {
-            bool needsRedraw = false;
-
-            if (isMouseOverClose || isMouseOverMinimize || isMouseOverMaximize)
+            foreach (var button in buttons)
             {
-                isMouseOverClose = false;
-                isMouseOverMinimize = false;
-                isMouseOverMaximize = false;
-                needsRedraw = true;
+                button.OnMouseLeave();
             }
+        }
 
-            if (needsRedraw)
+        private void MainForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            foreach (var button in buttons)
             {
-                Invalidate();
+                button.OnMouseClick(e.Location);
             }
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
-
-            int buttonWidth = 35;
-            int buttonHeight = 20;
-
-            minimizeButtonRect = new Rectangle(this.ClientSize.Width - 3 * buttonWidth, 0, buttonWidth, buttonHeight);
-            maximizeButtonRect = new Rectangle(this.ClientSize.Width - 2 * buttonWidth, 0, buttonWidth, buttonHeight);
-            closeButtonRect = new Rectangle(this.ClientSize.Width - buttonWidth, 0, buttonWidth, buttonHeight);
-
-            // Draw title bar background
-            //e.Graphics.FillRectangle(Brushes.DarkBlue, 0, 0,ClientSize.Width, buttonHeight);
-
-            // Draw minimize button
-            DrawButton(e.Graphics, minimizeButtonRect, "_", isMouseOverMinimize, Color.Black);
-            // Draw maximize/restore button
-            DrawButton(e.Graphics, maximizeButtonRect, isMaximized ? "❐" : "▢", isMouseOverMaximize, Color.Black);
-            // Draw close button
-            DrawButton(e.Graphics, closeButtonRect, "X", isMouseOverClose, Color.Red);
-        }
-
-        private void DrawButton(Graphics g, Rectangle rect, string text, bool isMouseOver, Color userColor)
-        {
-            Color buttonColor = isMouseOver ? Color.FromArgb(225, userColor) : Color.FromArgb(0, Color.Black);
-            using (Brush brush = new SolidBrush(buttonColor))
+            foreach (var button in buttons)
             {
-                g.FillRectangle(brush, rect);
-            }
-            //g.DrawRectangle(Pens.Black, rect);
-            using (Font font = new Font("Arial", 10, FontStyle.Bold))
-            {
-                SizeF textSize = g.MeasureString(text, font);
-                PointF location = new PointF(
-                    rect.X + (rect.Width - textSize.Width) / 2,
-                    rect.Y + (rect.Height - textSize.Height) / 2);
-                g.DrawString(text, font, Brushes.White, location);
-            }
-        }
-
-        protected override void OnMouseClick(MouseEventArgs e)
-        {
-            base.OnMouseClick(e);
-
-            if (closeButtonRect.Contains(e.Location))
-            {
-               Close();
-            }
-            else if (minimizeButtonRect.Contains(e.Location))
-            {
-               WindowState = FormWindowState.Minimized;
-            }
-            else if (maximizeButtonRect.Contains(e.Location))
-            {
-                if (isMaximized)
-                {
-                   WindowState = FormWindowState.Normal;
-                    isMaximized = false;
-                }
-                else
-                {
-                   WindowState = FormWindowState.Maximized;
-                    isMaximized = true;
-                }
-                Invalidate();
+                button.Draw(e.Graphics);
             }
         }
     }
